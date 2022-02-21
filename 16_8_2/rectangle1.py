c08cb8732db0906e00cc56d0f30239b439b61b48
EvgenyPolo <3670242@gmail.com>
2022-02-21 18:17:13 +0700

    Update 16.8

--- 16_8_2/rectangle1.py
+++ 16_8_2/rectangle1.py
@@ -9,21 +9,21 @@ print(rect_2.get_area())
 square_1 = Square(5)
 square_2 = Square(10)
 
-print(square_1.get_area_square(), square_2.get_area_square())
+print(square_1.get_area(), square_2.get_area())
 
 circ_1 = Circle(1)
 circ_2 = Circle(100)
 
-print(circ_1.get_area_circle(), circ_2.get_area_circle())
+print(circ_1.get_area(), circ_2.get_area(), Circle(10).get_area())
 
 
-figures = [rect_1, rect_2, square_1, square_2, circ_1, circ_2]
+figures = [Circle(3), Square(5), rect_1, rect_2, square_1, square_2, circ_1, circ_2]
 
 for figure in figures:
     if isinstance(figure,Square):
-        print("Площадь квадрата:", figure.get_area_square())
+        print("Площадь квадрата:", figure.get_area())
     elif isinstance(figure,Circle):
-        print(f"Площадь круга: {figure.get_area_circle()}")
+        print(f"Площадь круга: {figure.get_area()}")
     else:
         print("Площадь прямоугольника:", figure.get_area())
 
